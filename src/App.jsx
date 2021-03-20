87934eddc6e12a941c7509e86d765d183297a84f
arkadiibasovych <arkadiybasovich@gmai.com>
2021-03-20 15:37:46 +0200

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import { Route } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import HomeView from './views/HomeView';
 import AuthorsView from './views/Authors';
 import BooksView from './views/Books';
+import NotFoundView from './views/NotFoundView';
+import s from './App.module.css';
 
 const App = () => (
   <>
-    <Route exact path="/" component={HomeView} />
-    <Route exact path="/authors" component={AuthorsView} />
-    <Route exact path="/books" component={BooksView} />
+    <ul>
+      <li>
+        <NavLink
+          to="/authors"
+          className={s.navLink}
+          activeClassName={s.navLinkActive}
+        >
+          Authors
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/books"
+          className={s.navLink}
+          activeClassName={s.navLinkActive}
+        >
+          Books
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          exact
+          to="/"
+          className={s.navLink}
+          activeClassName={s.navLinkActive}
+        >
+          Home
+        </NavLink>
+      </li>
+    </ul>
+
+    <Switch>
+      <Route exact path="/" component={HomeView} />
+      <Route path="/authors" component={AuthorsView} />
+      <Route path="/books" component={BooksView} />
+      <Route component={NotFoundView} />
+    </Switch>
   </>
 );
 
